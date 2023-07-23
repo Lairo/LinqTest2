8297d9204a90d2242f5bcb776f5895916028f197
chajus1 <chajus1@googlemail.com>
2023-07-23 23:28:27 +0100

    Add project files.

---
+++ LinqTest/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqTest
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new();
+            for (int i = 1; i <= 99; i++)
+                numbers.Add(i);
+
+
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+
+
+            foreach (int i in firstAndLastFive)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
