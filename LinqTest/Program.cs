002d7eff58e90f21469bacb0bc0cbb2d6b8e21dc
chajus1 <chajus1@googlemail.com>
2023-07-27 22:04:45 +0100

    fix

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -8,11 +8,8 @@
             for (int i = 1; i <= 99; i++)
                 numbers.Add(i);
             IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
-
             foreach (int i in firstAndLastFive)
-            {
                 Console.Write($"{i} ");
-            }
         }
     }
 }
