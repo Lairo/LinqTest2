36f826f3dd300e7ebb14863d04dcb57259f2bc62
chajus1 <chajus1@googlemail.com>
2023-07-31 23:44:55 +0100

    removed Add Subtract to it's own file system

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -4,31 +4,23 @@
     {
         static void Main()
         {
-            //List<int> numbers = new();
-            //for (int i = 1; i <= 2; i++)
-            //    numbers.Add(i);
-            //IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(6));
-            //foreach (int i in numbers)
-            //    Console.Write($"{Enumerable.Range(0, 6).Average()} ");
+            List<int> numbers = new();
+            for (int i = 1; i <= 2; i++)
+                numbers.Add(i);
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(6));
+            foreach (int i in numbers)
+                Console.Write($"{Enumerable.Range(0, 6).Average()} ");
 
-            //int[] values = new[] { 0, 12, 44, 36, 92, 54, 13, 8 };
-            //IEnumerable<int> result =
-            //    from v in values
-            //    where v < 37
-            //    orderby -v
-            //    select v;
+            int[] values = new[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
 
-            //foreach (int v in result)
-            //    Console.Write($"{v} ");
-            //Console.ReadLine();
-
-            AddSubtract a = new AddSubtract() { Value = 5 }
-            .Add(5)
-            .Subtract(3)
-            .Add(9)
-            .Subtract(12);
-            Console.WriteLine($"Result: {a.Value}");
-
+            foreach (int v in result)
+                Console.Write($"{v} ");
+            Console.ReadLine();
         }
     }
 }
