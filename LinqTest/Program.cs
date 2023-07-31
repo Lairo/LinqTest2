615e02e49d28113863f748d585bf1fa39df4091d
chajus1 <chajus1@googlemail.com>
2023-07-31 23:22:09 +0100

    first linq

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -4,17 +4,23 @@
     {
         static void Main()
         {
-            List<int> numbers = new();
-            for (int i = 1; i <= 99; i++)
-                numbers.Add(i);
-            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(6));
-            foreach (int i in Enumerable.Range(1, 20))
-                Console.Write($"{i} ");
+            //List<int> numbers = new();
+            //for (int i = 1; i <= 2; i++)
+            //    numbers.Add(i);
+            //IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(6));
+            //foreach (int i in numbers)
+            //    Console.Write($"{Enumerable.Range(0, 6).Average()} ");
 
-            //IEnumerable<int> bob = numbers.Range(8, 5);
+            int[] values = new[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
 
-            //Console.WriteLine(Enumerable.Range(8, 5));
-
+            foreach (int v in result)
+                Console.Write($"{v} ");
+            Console.ReadLine();
         }
     }
 }
